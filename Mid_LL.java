24fe6c4fcb7cc33e723ed214a88a22db7ffbe32a
trigunach <97115262+trigunach@users.noreply.github.com>
2023-06-11 00:04:13 +0530

    Middle of a linkedlist

---
+++ Mid_LL.java
@@ -0,0 +1,10 @@
+public static Node findMiddle(Node head)
+{
+    Node slow = head;
+    Node fast = head ;
+    while(fast != null && fast.next != null){
+        slow = slow.next ;
+        fast = fast.next.next ;
+    }
+    return slow;
+}
